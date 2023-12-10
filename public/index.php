f3a07b8ec8e86b2d6197bf148239d1ed6f610eb6
Chris Williamson <chriswill0629@gmail.com>
2023-12-09 20:57:59 -0500

    Refactoring login and authenticator

--- public/index.php
+++ public/index.php
@@ -7,15 +7,10 @@
 	use Core\ValidationException;
 
 	$BASE_PATH = __DIR__ . '/../';
+	require $BASE_PATH . '/vendor/autoload.php';
 
 	require $BASE_PATH . "helpers/helpers.php";
 	require base_path("helpers/nav.helpers.php");
-
-	spl_autoload_register(function ($class) {
-		$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-		require base_path("{$class}.php");
-	});
-
 	require base_path('bootstrap.php');
 
 	$router = new Router();
